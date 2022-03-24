a07f1c7323162f1cfb1cedcb9b214dd519c36c16
erikgaliana <ekgaliana@gmail.com>
2022-03-24 23:31:55 +0100

    initial commit

---
+++ apps/whack-a-mole/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'whack-a-mole-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'whack-a-mole';
+}
