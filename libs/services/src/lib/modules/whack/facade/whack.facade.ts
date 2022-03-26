4f9ed6f04021af095388d80c10331093ff7ea40e
erikgaliana <ekgaliana@gmail.com>
2022-03-26 23:14:54 +0100

    feature(display-moles): add countDown$ and holeIndex$

--- libs/services/src/lib/modules/whack/facade/whack.facade.ts
+++ libs/services/src/lib/modules/whack/facade/whack.facade.ts
@@ -2,7 +2,7 @@
 import { Injectable } from '@angular/core';
 
 // RxJs
-import { Observable } from 'rxjs';
+import { exhaustMap, interval, map, Observable, take } from 'rxjs';
 
 // NgRx
 import { select, Store } from '@ngrx/store';
@@ -15,11 +15,32 @@ import { WhackActions } from '../store/actions/action-types';
 // Models
 import { GameModel } from '../../../models/whack.models';
 
+const INTERVAL_VALUE = 30;
 @Injectable()
 export class WhackFacade {
   // DATA OBSERVABLES
   gameData$: Observable<GameModel> = this.parentStore.pipe(
     select(fromSelectors.getGameData)
+  );
+
+  countDown$: Observable<number> = interval(1000).pipe(
+    map((index) => INTERVAL_VALUE - index),
+    take(INTERVAL_VALUE + 1)
+  );
+
+  holeIndexDelayed$: Observable<number> = this.countDown$.pipe(
+    exhaustMap((count) => {
+      const innerInterval = Math.floor(Math.random() * 3 + 1);
+
+      return interval(innerInterval * 1000).pipe(
+        map(() => {
+          const randomHole = Math.floor(Math.random() * 6);
+
+          return randomHole;
+        }),
+        take(1)
+      );
+    })
   );
 
   isGameDataLoaded$: Observable<boolean> = this.parentStore.pipe(
