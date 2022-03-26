32c7e881862a6c4c09caad6df13a04c6c24c24d1
erikgaliana <ekgaliana@gmail.com>
2022-03-26 23:16:11 +0100

    feature(display-moles): add IndexMole prperty

--- apps/whack-a-mole/src/app/app.component.ts
+++ apps/whack-a-mole/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WhackFacade } from '@whack-a-mole/services';
+import { Subject, take, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'whack-a-mole-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'whack-a-mole';
+
+  indexMole: number;
+
+  holesArray: number[] = [0, 0, 0, 0, 0, 0];
+
+  destroyed$: Subject<boolean> = new Subject<boolean>();
+  constructor(public whackFacade: WhackFacade) {}
+
+  ngOnInit(): void {
+    this.whackFacade.holeIndexDelayed$
+      .pipe(takeUntil(this.destroyed$))
+      .subscribe((index) => (this.indexMole = index));
+  }
+
+  ngOnDestroy(): void {
+    this.destroyed$.next(true);
+    this.destroyed$.complete();
+  }
 }
