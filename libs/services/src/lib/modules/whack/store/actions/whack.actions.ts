6c36701113db344dfe0d5c593df2b516dc0819d3
erikgaliana <ekgaliana@gmail.com>
2022-04-03 20:17:49 +0200

    feature(multiple-moles): add loadData and updateMoles

--- libs/services/src/lib/modules/whack/store/actions/whack.actions.ts
+++ libs/services/src/lib/modules/whack/store/actions/whack.actions.ts
@@ -1,6 +1,19 @@
 // NgRx
 import { createAction, props } from '@ngrx/store';
 
+// Models
+import { Mole } from '../../../../models/whack-models';
+
+export const loadData = createAction(
+  '[Whack a Mole] Load Initial Data',
+  props<{ molesList: Mole[] }>()
+);
+
+export const updateMoles = createAction(
+  '[Whack a Mole] Update Moles',
+  props<{ molesList: Mole[] }>()
+);
+
 export const updateScore = createAction(
   '[Whack a Mole] Update Score ',
   props<{ score: number }>()
