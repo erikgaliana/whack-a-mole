a03bd344e88faad3511475ada9ab981b3f052405
erikgaliana <ekgaliana@gmail.com>
2022-04-03 16:53:44 +0200

    refactor(multiple-moles): add game view component

--- apps/whack-a-mole/src/app/app.module.ts
+++ apps/whack-a-mole/src/app/app.module.ts
@@ -3,26 +3,32 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
+// Router
+import { AppRoutingModule } from './app-routing.module';
+
 // NgRx
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 // Store
-import { WhackFacade, WhackStoreModule } from '@whack-a-mole/services';
+import { WhackStoreModule } from '@whack-a-mole/services';
+import { GameViewModule } from './game/game.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
+    GameViewModule,
     BrowserModule,
     WhackStoreModule,
+    AppRoutingModule,
     StoreModule.forRoot({}),
     EffectsModule.forRoot([]),
     StoreDevtoolsModule.instrument({
       maxAge: 30,
     }),
   ],
-  providers: [WhackFacade],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
